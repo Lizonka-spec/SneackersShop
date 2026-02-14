dfceb43b550c0a4339290a7500916675fc9bb958
Lizonka-spec <proudoffyou01@gmail.com>
2026-02-14 21:04:24 +0300

    delete footer

--- src/components/MainLayOut.jsx
+++ src/components/MainLayOut.jsx
@@ -1,11 +1,10 @@
-import { Footer, Navigation } from "./index";
+import { Navigation } from "./index";
 import styles from "../styles/main.module.css";
 export const MainLayOut = ({ children }) => {
   return (
     <div className={styles.container}>
       <Navigation />
       <main className={styles.main}>{children}</main>
-      <Footer />
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { Navigation } from "./Navigation";
 export { MainLayOut } from "./MainLayOut";
-export { Footer } from "./Footer";
 export { Card } from "./Card";
 export { Searching } from "./Searching";
 export { PrivateRoute } from "./PrivateRoute";
